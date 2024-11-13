5815fde97782afdcf8c211f1c0b02f75bb906ab3
Pior Barankiewicz <piotr.barankiewicz@gmail.com>
2024-11-13 11:07:04 +0100

    repository abstraction

---
+++ LevelUpCSharp.Domain/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelUpCSharp
+{
+	internal interface IRepository<TKey, TItem>
+    {
+        void Add(TKey key, TItem item);
+
+        //bool Exists(TKey key);
+
+        //IEnumerable<TItem> GetAll();
+
+        //TItem Get(TKey key);
+
+        //void Remove(TKey key);
+    }
+}

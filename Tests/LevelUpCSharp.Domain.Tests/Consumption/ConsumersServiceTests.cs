c4025ad3741cfb911db394418250f3fbf70dda04
Pior Barankiewicz <piotr.barankiewicz@gmail.com>
2024-11-14 11:50:01 +0100

    test cases setup

---
+++ Tests/LevelUpCSharp.Domain.Tests/Consumption/ConsumersServiceTests.cs
@@ -0,0 +1,25 @@
+using LevelUpCSharp.Testing.Data;
+using NUnit.Framework;
+
+namespace LevelUpCSharp.Domain.Tests.Consumption
+{
+    public class ConsumersServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Create_InvalidName_Failed()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Create_ValidName_Success()
+        {
+            Assert.Fail();
+        }
+    }
+}
